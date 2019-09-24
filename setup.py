27217c128693c50d2a26ffe15bfae6aa3d7cc206
chengsy <chengsy@cmgos.com>
2019-09-24 10:39:17 +0800

    add code

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ twine upload dist/*
 
 setup(
     name='runtogether',
-    version='0.0.3.6', # 项目版本
-    author='小成', # 项目作者
-    author_email='499938136@qq.com', # 作者email
-    url='https://github.com/csy5621080/runtogether', # 项目代码仓库
-    description='多端口代理/类gunicorn', # 项目描述
-    packages=['runtogether'], # 包名
+    version='0.0.3.6',  # 项目版本
+    author='小成',  # 项目作者
+    author_email='499938136@qq.com',  # 作者email
+    url='https://github.com/csy5621080/runtogether',  # 项目代码仓库
+    description='多端口代理/类gunicorn',  # 项目描述
+    packages=['runtogether'],  # 包名
     install_requires=['psutil', 'argparse'],
     entry_points={'console_scripts': [
         'rtg = runtogether.command:main'
